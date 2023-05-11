936b8ae0b5271c01b0e6267409c47be2b2bfdbee
doflsld <yerin0827@gmail.com>
2023-05-11 16:58:07 +0900

    transport co 값, 5초마다 갱신 수정

--- src/main/resources/static/js/sens_data.js
+++ src/main/resources/static/js/sens_data.js
@@ -1,7 +1,4 @@
     function pulse() {
-        $('#t_temp').load(location.href+' #t_temp');
-        $('#t_humi').load(location.href+' #t_humi');
-        $('#t_co').load(location.href+' #t_co');
         $('#e_temp').load(location.href+' #e_temp');
         $('#e_humi').load(location.href+' #e_humi');
         $('#e_co2').load(location.href+' #e_co2');
